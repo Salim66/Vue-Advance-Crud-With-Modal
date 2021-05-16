d2597aedd4ac1bc558294028d41181f0070e6ee1
Salim66 <salimhasanriad8@gmail.com>
2021-05-16 14:58:03 +0600

    Get all data and show into crud

--- inc/user.php
+++ inc/user.php
@@ -4,4 +4,22 @@
 // Server Connection
 $conn = new mysqli('localhost', 'root', '', 'vue-advance-crud');
 
+//get action and assign to another variable
+$action = "read";
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+}
 
+//when action value is red then all users data get into server
+if ($action == 'read') {
+    //get all data query
+    $data_all = $conn->query("SELECT * FROM users ORDER BY id DESC ");
+
+    $data = [];
+
+    //all data fetch
+    while ($user = $data_all->fetch_assoc()) {
+        array_push($data, $user);
+    }
+    echo json_encode($data);
+}
